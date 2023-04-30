f908604c1dd0bb31c8cff018a2fc684c3daf802b
douglasfabricioedgardo <douglasfabricioedgardo@gmail.com>
2023-04-29 23:32:23 -0600

    week

--- Week2/scripts/scripts.js
+++ Week2/scripts/scripts.js
@@ -1,7 +1,30 @@
-let date = new Date()
-let year = date.getFullYear()
-document.getElementById("currentYear").innerHTML = year
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let monthsnames = [
+    "January",
+    "Febrary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 
-let getTimestamp = Date.now()
-let timestamp = new Date(getTimestamp)
-document.getElementById("timestamp").innerHTML = "Last Updated: " + timestamp
+let d = new Date();
+let Dayname = daynames[d.getDay()];
+let Monthname = monthsnames[d.getMonth()];
+let fulldate = Dayname + ", " + Monthname + ", " + d.getDate() + ", " +
+    d.getFullYear();
+document.getElementById("current").textContent = fulldate;
