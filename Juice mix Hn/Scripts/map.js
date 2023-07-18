534355f9ebc73f875532ff00db8ea7c55aae99fa
douglasfabricioedgardo <douglasfabricioedgardo@gmail.com>
2023-07-18 00:37:34 -0600

    ss

--- Juice mix Hn/Scripts/map.js
+++ Juice mix Hn/Scripts/map.js
@@ -2,11 +2,11 @@
 
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 15.68333, lng: -86 },
+    center: { lat: 15.650770, lng:  -86.012248 },
     zoom: 10,
   });
   const marker = new google.maps.Marker({
-    position: { lat: 15.68333, lng: -86 },
+    position: { lat: 15.650770, lng:  -86.012248 },
     map: map,
     title: "Tocoa",
   });
