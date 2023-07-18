fa4e7cccfd58974b207c19daff107a9463f9f46e
douglasfabricioedgardo <douglasfabricioedgardo@gmail.com>
2023-07-18 01:54:10 -0600

    ss

--- Juice mix Hn/Scripts/welcomemessage.js
+++ Juice mix Hn/Scripts/welcomemessage.js
@@ -40,7 +40,7 @@ function displayDate() {
   // Load the Google Maps API script and call the initMap function when it's loaded
   (function () {
     const script = document.createElement("script");
-    script.src = `https://api.openweathermap.org/data/2.5/weather?q=Tocoa%2C+Hn&appid=$eb62be483b069ca0c752ef1f2c028508}&units=imperial`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyC7PC1BDod4JQcuPcOqOGdjEfz9YFhQUKo&callback=initMap`;
     script.defer = true;
     script.async = true;
     document.body.appendChild(script);
